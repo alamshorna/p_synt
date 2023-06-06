ac86ad703e06dc268cbf808e9abe42a7a95c5c5a
Shorna Alam <shornaalam@dhcp-10-31-27-143.dyn.MIT.EDU>
2023-06-06 12:57:30 -0400

    starting files

---
+++ prot_model.py
@@ -0,0 +1,22 @@
+# import math
+# import os
+
+# import torch
+# from torch import nn, Tensor
+# from torch.nn import TransformerEncoder, TransformerEncoderLayer
+
+# def PositionalEncoding(model):
+
+# class TransformerModel(nn.Module):
+#     def __init__(self, model):
+#         self.model_type = "transformer"
+#         self.positional_encoding = PositionalEncoding(model)
+
+
+
+
+
+
+
+# # def data_process():
+
