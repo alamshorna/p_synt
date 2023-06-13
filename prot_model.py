4deda29cf58aee11675ab984a9da446d7983bcd2
Shorna Alam <shorna@mit.edu>
2023-06-13 16:31:58 -0400

    :o she trains

--- prot_model.py
+++ prot_model.py
@@ -32,11 +32,21 @@ class PositionalEncoding(nn.Module):
         pe[:, 0, 0::2] = torch.sin(position * div_term)
         pe[:, 0, 1::2] = torch.cos(position * div_term)
         size = pe.size()
+        #print('pe', size)
         self.pe = pe.reshape(size[2], size[0], size[1])
-    def forward(self, embedding):
-        embedding = embedding + self.pe[:embedding.size(0)]
-        return embedding
+        # print('pe', self.pe.size())
 
+    def forward(self, embedding):
+        embedding = embedding.t().unsqueeze(2)
+        embedding = embedding + self.pe[:embedding.size(0)]
+        print('pe', self.pe.size())
+        print('embedding_forward', embedding.size())
+        #print(self.pe.size(), embedding.size(0), self.pe[:embedding.size(0)])
+        #print(embedding.size(0))
+        print(self.pe[:embedding.size(0)].size())
+        #print(embedding.size())
+        return embedding.squeeze(2).t()
+    
 def number_sequences(fasta_file):
     fasta_data = list(SeqIO.parse(open(fasta_file), 'fasta'))
     return len(fasta_file)
@@ -57,8 +67,10 @@ class TransformerModel (nn.Module):
         self.ninp = number_sequences(fasta_file)
         self.ntoken = len(token_index_aa) if alphabet_string == 'aa' else len(token_index_codon)
         self.embedding = nn.Embedding(self.ntoken, 512)
-        self.decoder = nn.Linear(self.ninp, self.ntoken)
+        #self.decoder = nn.Linear(hidden_dimensions, output)
+        self.decoder = nn.Linear(self.d_model, self.ntoken)
         self.init_weights()
+        self.satya = 'satya'
 
     def init_weights(self):
         initrange = 0.1
@@ -68,9 +80,12 @@ class TransformerModel (nn.Module):
 
     def forward(self, src):
         src = self.embedding(src) * math.sqrt(self.ninp)
-        
+        # print('embedding', src.size())
         src = self.pos_encoder(src)
+        # print('positional_encoding', src.size())
         output = self.transformer_encoder(src)
+        # print('output', output)
+        # print('output', output.size())
         output = self.decoder(output)
         return output
     
@@ -95,13 +110,14 @@ def train(model: nn.Module):
     model.train()
     epochs = 100
     total_loss = 0
-    log_interval = 10000
+    log_interval = 5
     start_time = time.time()
     learning_rate = 5.0
     loss_function = nn.CrossEntropyLoss()
     optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
     data = data_process(model.fasta_file, model.d_model, model.alphabet, do_mask = True)
     correct_data = data_process(model.fasta_file, model.d_model, model.alphabet, do_mask = False)
+    start_time = time.time()
 
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.95)
     
@@ -110,18 +126,36 @@ def train(model: nn.Module):
         for batch in data:
             optimizer.zero_grad()
             #print(model)
-
+            
             out = model(batch)
             print(i)
-            print(model.decoder(out))
+            print(batch)
+            print(out)
             i+=1
-            # loss = loss_function()
-            # total_loss += loss
+            loss = loss_function(out, batch)
+            print(loss)
+            total_loss += loss
 
-            # loss.backward()
-            # optimizer.step()
+            optimizer.zero_grad()
+            #loss.backward()
+            #torch.nn.utils.clip_grad_norm_(model.parameters(), 0.01)
+            print(loss)
+            optimizer.step()
+
+            
+            # if i % log_interval == 0 and i > 0:
+            #     lr = scheduler.get_last_lr()[0]
+            #     ms_per_batch = (time.time() - start_time) * 1000 / log_interval
+            #     cur_loss = total_loss / log_interval
+            #     print(cur_loss)
+            #     ppl = math.exp(cur_loss)
+            #     print(f'| epoch {epoch:3d} | '
+            #         f'lr {lr:02.2f} | ms/batch {ms_per_batch:5.2f} | '
+            #         f'loss {cur_loss:5.2f} | ppl {ppl:8.2f}')
+            #     total_loss = 0
+            #     start_time = time.time()
         
-        if epoch % log_interval == 0 or epoch == 0:
+        if epoch % log_interval == 5 or epoch == 0:
             print("Epoch: {} -> loss: {}".format(epoch+1, total_loss/(len(data)*epoch+1)))
 
 def evaluate(model: nn.Module, eval_data: Tensor):
